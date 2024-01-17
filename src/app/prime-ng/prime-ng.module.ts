1989848ae21b8f83972f08307ae267cbaf970d2b
Carlos jesus <carlosjesus.moralespica2000@gmail.com>
2024-01-17 12:08:23 +0100

    Catalogo hecho / responsive

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common'
 
 import { ButtonModule } from 'primeng/button'
 import { MenubarModule } from 'primeng/menubar'
+import { CardModule } from 'primeng/card'
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [ButtonModule, MenubarModule],
+  exports: [ButtonModule, MenubarModule, CardModule],
 })
 export class PrimeNgModule {}
--- src/app/products/pages/catalogo-home/catalogo-home.component.ts
+++ src/app/products/pages/catalogo-home/catalogo-home.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-catalogo-home',
   templateUrl: './catalogo-home.component.html',
-  styleUrls: ['./catalogo-home.component.scss']
+  styleUrls: ['./catalogo-home.component.scss'],
 })
 export class CatalogoHomeComponent {
-
+  public catalogoInfo = [
+    {
+      id: 0,
+      title: 'new in',
+      src: 'assets/catalogo/MADKAT_newin.png',
+    },
+    {
+      id: 1,
+      title: 'madkat on tour',
+      src: 'assets/catalogo/MADKAT_madkatOnTour.png',
+    },
+    {
+      id: 2,
+      title: 'originals',
+      src: 'assets/catalogo/MADKAT_originals.png',
+    },
+    {
+      id: 3,
+      title: 'sudaderas',
+      src: 'assets/catalogo/MADKAT_hoodies.png',
+    },
+    {
+      id: 4,
+      title: 'camisetas',
+      src: 'assets/catalogo/MADKAT_tshirts.png',
+    },
+    {
+      id: 5,
+      title: 'accesorios',
+      src: 'assets/catalogo/MADKAT_accesories.png',
+    },
+  ]
 }
