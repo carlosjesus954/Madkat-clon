c49ef9c313d964237a1ba09b4cfbd3e6020f1e97
Carlos jesus <carlosjesus.moralespica2000@gmail.com>
2024-02-05 18:38:58 +0100

    Maquetacion finalizada falta , responsive para desktop mas logica y guards

--- src/app/products/pages/carrito-page/carrito-page.component.ts
+++ src/app/products/pages/carrito-page/carrito-page.component.ts
@@ -21,7 +21,4 @@ export class CarritoPageComponent implements OnInit {
       this.precioTotal = price
     })
   }
-  isNotLast(index: number): boolean {
-    return index < this.carritoActual.length - 1
-  }
 }
